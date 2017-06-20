7a6a7549720543aa53a8ea5303b1290f0a4acc0b
Bruno Fidelis <bruno.fidelis004@gmail.com>
2017-06-20 01:55:34 -0300

    atom events

--- src/todo/core.cljs
+++ src/todo/core.cljs
@@ -1,11 +1,32 @@
 (ns todo.core
-  (:require ))
+  (:require [goog.dom :as dom]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
-;; https://clojure.org/reference/atoms
-(defonce app-state (atom {:text "Hello world!"}))
+(def app-root (dom/getElement "app"))
 
+;; https://clojure.org/reference/atoms
+(defonce app-state 
+  (atom {:todos ["Baixar JDK" "Baixar Leiningem"] }))
+
+(defn set-state [new-state]
+  (swap! app-state conj new-state))
+
+
+(defn list-el [list]
+  (str "<ul>" 
+    (string/join "" 
+      (map (fn [x] (str "<li>" x "</li>")) list)) "</ul>"))
+
+(defn render-list []
+  (set! (.-innerHTML app-root) (list-el (:todos @app-state))))
+
+
+(add-watch app-state :on-text-change render-list)
+
+
+(render-list)
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
