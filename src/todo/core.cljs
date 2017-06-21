2051a8aa19856ae4944d374fdab676bfacacee47
Bruno Fidelis <bruno.fidelis004@gmail.com>
2017-06-20 23:06:45 -0300

    adding events

--- src/todo/core.cljs
+++ src/todo/core.cljs
@@ -1,16 +1,19 @@
 (ns todo.core
   (:require [goog.dom :as dom]
+            [goog.events :as events]
             [clojure.string :as string]))
 
 (enable-console-print!)
 
 (def app-root (dom/getElement "app"))
+(def app-item-btn (dom/getElement "sendItem"))
+(def app-nitem (dom/getElement "newItem"))
 
 ;; https://clojure.org/reference/atoms
 (defonce app-state 
   (atom {:todos ["Baixar JDK" "Baixar Leiningem"] }))
 
-(defn set-state [new-state]
+(defn set-state! [new-state]
   (swap! app-state conj new-state))
 
 
@@ -22,11 +25,15 @@
 (defn render-list []
   (set! (.-innerHTML app-root) (list-el (:todos @app-state))))
 
+(defn add-item-to-list [e]
+  (set-state! {:todos (conj (:todos @app-state) (.-value app-nitem))}))
 
 (add-watch app-state :on-text-change render-list)
 
-
 (render-list)
+
+(events/listen app-item-btn "click" add-item-to-list)
+
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
