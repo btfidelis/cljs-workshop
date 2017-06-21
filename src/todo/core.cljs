ee450bb4f383be3024b8f15bfb852779984262bc
Bruno Fidelis <bruno.fidelis004@gmail.com>
2017-06-21 17:01:07 -0300

    ajax

--- src/todo/core.cljs
+++ src/todo/core.cljs
@@ -2,21 +2,53 @@
   (:require [goog.dom :as dom]
             [goog.events :as events]
             [secretary.core :as secretary :refer-macros [defroute]])
-  (:import goog.History))
+  (:import goog.History
+           goog.Uri
+           goog.net.Jsonp))
 
 
 (enable-console-print!)
 
 (def app-root (dom/getElement "app"))
+(defonce fixer-uri "http://api.fixer.io/latest")
+(def home-view 
+  (str "<h1>Taxas de intercâmbio</h1>"
+       "<div id=\"results\"></div>"))
 
-(defn set-html! [content]
-  (set! (.-innerHTML app-root) content))
+(defn set-html! 
+  ([content] (set! (.-innerHTML app-root) content))
+  ([content el] (set! (.-innerHTML el) content)))
+
+(defn get-from-fixer 
+  [path callback]
+  (let [req (Jsonp. (Uri. (str fixer-uri path)))]
+    (.send req nil callback)))
+
+(defn render-results [res el]
+  (let [results (js->clj res)]
+    (set-html! 
+      (str "<p> Tipo: " (get results "base") "</p>"
+           "<p> Atualizado em: " (get results "date") "</p>"
+            (reduce 
+              (fn [acc curr]
+                (let [[coin value] curr]
+                  (str acc "<div>" coin ":" value "</div>"))) 
+                    "" (get results "rates"))) el)))
+
+
+
+(defn render-fix [path] 
+  (let [on-response 
+    (fn [r] (render-results r (dom/getElement "results")))] 
+      (get-from-fixer path on-response)))
 
 (defroute home "/" [] 
-  (set-html! "<h1>Bem vindo!</h1>"))
+  (set-html! home-view)
+  (render-fix ""))
 
-(defroute perfil "/perfil/:user" [user] 
-  (set-html! (str "<h1> Olá " user "</h1>")))
+(defroute perfil "/moeda/:type" [type] 
+  (set-html! (set-html! home-view))
+  (render-fix (str "?base=" type)))
 
 (secretary/set-config! :prefix "#")
 
